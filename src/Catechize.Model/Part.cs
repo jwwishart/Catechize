8ce50744a7f10320421a890147cf5d0c44def572
Justin Wishart <jwwishart@example.com>
2011-03-01 19:11:35 +1000

    Added basic Part class and repository

---
+++ src/Catechize.Model/Part.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Catechize.Model
+{
+    public class Part
+    {
+        public int PartID {get; set;}
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+    }
+}
