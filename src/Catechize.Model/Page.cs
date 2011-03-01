02d38f73a990c1abb15a68172aef972ba888fe70
Justin Wishart <jwwishart@example.com>
2011-03-01 19:13:51 +1000

    Added basic Page class and repository

---
+++ src/Catechize.Model/Page.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Catechize.Model
+{
+    public class Page
+    {
+        public int PageID { get; set; }
+        public int CourseID { get; set; }
+        public int PartID { get; set; }
+        public int Ordinal { get; set; }
+    }
+}
---
+++ src/Catechize.Services/IPageRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Catechize.Model;
+
+namespace Catechize.Services
+{
+    public interface IPageRepository
+    {
+        void Create(Page newPage);
+        void Save(Page page);
+        Page GetById(int pageID);
+    }
+}
+
+namespace Catechize.Services.SqlServer
+{
+    public class PageRepository : IPageRepository
+    {
+        public void Create(Page newPage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Page page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Page GetById(int pageID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
