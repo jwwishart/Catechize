118bca916245fe178b75f9c9b44771017230c7ab
Justin Wishart <jwwishart@example.com>
2011-03-01 23:27:46 +1000

    work on IDataAccess - brainstorming

---
+++ src/Catechize.Services/IDataAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Common;
+
+namespace Catechize.Services
+{
+    public interface IDataAccess
+    {
+        // Internal helper for Sql server
+        void ExecuteNonQuery(string query);
+        void ExecuteNonQuery(DbCommand command);
+        object ExecuteScalar(string query);
+        object ExecuteScalar(DbCommand command);
+
+        // Factory?
+        DbConnection CreateConnect(string connectionString = null);
+        DbCommand CreateCommand(string commandText = null);
+        DbTransaction CreateTransaction(); 
+    }
+}
