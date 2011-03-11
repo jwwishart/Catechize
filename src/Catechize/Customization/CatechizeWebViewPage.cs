d32921906e4eb117f82c3600cfc53e52d15113c5
Justin Wishart <jwwishart@example.com>
2011-03-12 08:58:15 +1000

    Configured Web.config page base type to new one, added reference link to Links.txt

--- src/Catechize/Customization/CatechizeWebViewPage.cs
+++ src/Catechize/Customization/CatechizeWebViewPage.cs
@@ -7,7 +7,7 @@ using Catechize.Helpers;
 
 namespace Catechize.Customization
 {
-    public class CatechizeWebViewPage : WebViewPage
+    public abstract class CatechizeWebViewPage : WebViewPage
     {
         public UrlGenerationHelper UrlGen { get; set; }
 
@@ -15,6 +15,17 @@ namespace Catechize.Customization
         {
             base.InitHelpers();
             UrlGen = new UrlGenerationHelper(base.ViewContext, this);
+        }
+    }
+
+    public abstract class CatechizeWebViewPage<T> : CatechizeWebViewPage
+    {
+        public UrlGenerationHelper<T> UrlGen { get; set; }
+
+        public override void InitHelpers()
+        {
+            base.InitHelpers();
+            UrlGen = new UrlGenerationHelper<T>(base.ViewContext, this);
         }
     }
 }
--- src/Catechize/Helpers/UrlGenerationHelper.cs
+++ src/Catechize/Helpers/UrlGenerationHelper.cs
@@ -34,3 +34,37 @@ public class UrlGenerationHelper
         private set;
     }
 }
+
+public class UrlGenerationHelper<T>
+{
+    public UrlGenerationHelper(ViewContext viewContext, IViewDataContainer viewDataContainer)
+        : this(viewContext, viewDataContainer, RouteTable.Routes)
+    {
+    }
+
+    public UrlGenerationHelper(ViewContext viewContext, IViewDataContainer viewDataContainer
+        , RouteCollection routeCollection)
+    {
+        this.ViewContext = viewContext;
+        this.ViewData = new ViewDataDictionary<T>(viewDataContainer.ViewData);
+        this.RouteCollection = routeCollection;
+    }
+
+    public ViewDataDictionary<T> ViewData
+    {
+        get;
+        private set;
+    }
+
+    public ViewContext ViewContext
+    {
+        get;
+        private set;
+    }
+
+    public RouteCollection RouteCollection
+    {
+        get;
+        private set;
+    }
+}
